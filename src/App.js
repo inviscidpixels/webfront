095f88c5008e03aed23afe3001bbb25263abee2b
inviscid pixels <inviscidpixels@protonmail.com>
2022-02-14 16:47:41 -0500

    Added styled, webfontloader and media-query for response css, updated hello world

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import devices from './media-query';
+import WebFont from 'webfontloader';
+
+import styled from 'styled-components';
 
 export default function App() {
+
+    useEffect(() => {
+        WebFont.load({
+            google: {
+                families: [ 'Inter', 'Spectral', 'Monoton' ]
+            } 
+        });
+        }, []);
     return (
     <>
-        hello world
+        <Styled>
+            oh the inviscid pixels...
+        </Styled>
+
     </>
     )
 }
+
+const Styled = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    font-family: 'Spectral';
+    font-size: 4em;
+    user-select: none;
+
+    
+    @media ${devices.mobileXS} {
+        font-size: 1.25em;
+    }
+
+    @media ${devices.mobileS} {
+        font-size: 1.25em;
+    }
+
+    @media ${devices.mobileM} {
+        font-size: 1.75em;
+    }
+
+    @media ${devices.mobileL} {
+        font-size: 1.5em;
+    }
+
+    @media ${devices.tablet} {
+        font-size: 3em;
+    }
+
+    @media ${devices.laptop} {
+        font-size: 5em;
+    }
+
+    @media ${devices.laptopL} {
+        font-size: 8em;
+    }
+
+    @media ${devices.desktop} {
+        font-size: 12em;
+    }
+
+
+ 
+`
---
+++ src/media-query.js
@@ -0,0 +1,23 @@
+const sizes = {
+    mobileXS: '280px',
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktop: '2560px',
+};
+
+const devices = {
+    mobileXS: `(min-width: ${sizes.mobileXS})`,
+    mobileS: `(min-width: ${sizes.mobileS})`,
+    mobileM: `(min-width: ${sizes.mobileM})`,
+    mobileL: `(min-width: ${sizes.mobileL})`,
+    tablet: `(min-width: ${sizes.tablet})`,
+    laptop: `(min-width: ${sizes.laptop})`,
+    laptopL: `(min-width: ${sizes.laptopL})`,
+    desktop: `(min-width: ${sizes.desktop})`,
+};
+
+export default devices;
